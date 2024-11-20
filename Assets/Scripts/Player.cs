de3ed7f43ac2307f37e935751fdeb5b5cfe3fcd6
Barak Finkel <barak.finkel11@gmail.com>
2024-11-20 19:13:36 +0200

    Fixed according to .yml file

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,33 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-    
+
     private float xInput;
     private int facingDir = 1;
     private bool facingRight = true;
     private bool isGrounded;
-    
+
     [Header("Movement")]
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
-    
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
     [Header("Collision Info")]
-    [SerializeField] private float groundCheckDist;
-    [SerializeField] private LayerMask whatIsGround;
-    
-    void Start()
+    [SerializeField]
+    private float groundCheckDist;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
         Movement();
         FlipController();
@@ -43,41 +49,49 @@ public class Player : MonoBehaviour
     {
         xInput = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump"))
+        {
             Jump();
+        }
     }
 
     private void Movement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 
     private void Jump()
     {
-        if(isGrounded)
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void AnimatorControllers()
     {
-        anim.SetFloat("yVelocity", rb.linearVelocity.y);
-        bool isMoving = rb.linearVelocity.x != 0;
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        bool isMoving = rb.velocity.x != 0;
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
     }
 
     private void Flip()
     {
-        facingDir *= -1; 
+        facingDir *= -1;
         facingRight = !facingRight;
-        transform.Rotate(0,180,0);
+        transform.Rotate(0, 180, 0);
     }
 
     private void FlipController()
     {
-        if (rb.linearVelocity.x > 0 && !facingRight)
+        if (rb.velocity.x > 0 && !facingRight)
+        {
             Flip();
-        else if (rb.linearVelocity.x < 0 && facingRight)
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
             Flip();
+        }
     }
 
     private void OnDrawGizmos()
